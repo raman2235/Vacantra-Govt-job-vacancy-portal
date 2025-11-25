24adada5623425164a42869d3cf70a7f961c76a0
raman2235 <deepraman2235@gmail.com>
2025-11-25 17:54:49 +0530

    fix prisma env config

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from 'prisma/config';
+// server/prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
 });
